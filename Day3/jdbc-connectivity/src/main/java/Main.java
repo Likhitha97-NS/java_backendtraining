bc2dc018fbd2ffecd8c7b934316b2725c2128492
likhitha.ns <likhitha.ns@collabera.com>
2022-01-06 11:59:23 +0530

    database:CRUD update  satement operation

---
+++ Day3/jdbc-connectivity/src/main/java/Main.java
@@ -0,0 +1,31 @@
+import db.Connectivity;
+import service.DbService;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Connectivity connectivity = new Connectivity();
+            DbService service = new DbService(connectivity.getConnection());
+
+            //int cnt = service.create(
+            //  3,
+            //"pqr",
+            //Date.valueOf(LocalDate.now()),
+            // false
+            //);
+            //if (cnt > 0) {
+            //  System.out.println("Employee created succesfully");
+            //}
+
+            service.find();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
