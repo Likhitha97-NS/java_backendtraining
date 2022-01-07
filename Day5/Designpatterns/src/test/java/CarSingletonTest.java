432f67dbf7b65d278a787188ef197ee4910fdacf
likhitha.ns <likhitha.ns@collabera.com>
2022-01-07 12:25:06 +0530

    checking singleton instance

---
+++ Day5/Designpatterns/src/test/java/CarSingletonTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import singleton.CarSingleton;
+
+public class CarSingletonTest
+{
+    @DisplayName("check for instance i ssingleton")
+    @Test
+    void testCarSingleton()
+    {
+        var c1= CarSingleton.newInstance();
+        var c2=CarSingleton.newInstance();
+        Assertions.assertEquals(
+                c1.hashCode(),
+                c2.hashCode()
+        );
+    }
+}
