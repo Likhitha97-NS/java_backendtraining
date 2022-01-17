96b1117bb992240b870cf74ced1e387b6ff63709
likhitha.ns <likhitha.ns@collabera.com>
2022-01-17 14:35:57 +0530

    pqr-microservice

---
+++ week-3/Day-1/pqr-microservice/src/main/java/com/pqrmicroservice/pqrmicroservice/PqrMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pqrmicroservice.pqrmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqrMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqrMicroserviceApplication.class, args);
+	}
+
+}
